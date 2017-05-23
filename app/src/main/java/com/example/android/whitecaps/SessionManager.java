b1e603dbf4cf3b86a871ffada94475a6a8cf317d
harshu1803 <harshitanarnoli18@gmail.com>
2017-05-23 17:56:00 +0530

    Update SessionManager.java

--- app/src/main/java/com/example/android/whitecaps/SessionManager.java
+++ app/src/main/java/com/example/android/whitecaps/SessionManager.java
@@ -8,7 +8,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.content.SharedPreferences.Editor;
 
-public class SessionManager {
+public class SessionManager{
 
 
     // Shared Preferences reference
